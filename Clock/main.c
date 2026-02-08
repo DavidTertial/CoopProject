a2cc389c3c46fef765ec31c34dd9476492c656df
DavidTertial <davidtertial@gmail.com>
2026-02-07 21:25:12 -0400

    Refactor main function for better time display
    Refactor main function to improve time display logic.

--- Clock/main.c
+++ Clock/main.c
@@ -1,15 +1,24 @@
-#include<stdio.h>
-#include<time.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main ()
+int main(void)
 {
-    int hour, minute, second;
-    hour = minute = second = 0;
-    
-    while(1){
-        
+    while (1) {
+        time_t now = time(NULL);
+        struct tm *t = localtime(&now);
+
+        int hour = t->tm_hour;
+        int minute = t->tm_min;
+        int second = t->tm_sec;
+
+        // Print time in HH:MM:SS format (24-hour clock)
+        printf("\r%02d:%02d:%02d", hour, minute, second);
+        fflush(stdout);
+
+        sleep(1);
     }
-    
+
+    return 0;
 }
